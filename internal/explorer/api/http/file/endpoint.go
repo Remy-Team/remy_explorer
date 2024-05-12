048d1819f588a6fe82ef5ddf551a6b4b7483962f
Fascinator <fascinator2904@gmail.com>
2024-05-13 01:15:27 +0300

    Add new endpoint for file domain

---
+++ internal/explorer/api/http/file/endpoint.go
@@ -0,0 +1,103 @@
+package file
+
+import (
+	"context"
+	"github.com/go-kit/kit/endpoint"
+	"remy_explorer/internal/explorer/service/file"
+)
+
+// Endpoints holds all Go kit endpoints for file operations
+type Endpoints struct {
+	// For files
+	CreateFile         endpoint.Endpoint
+	GetFileByID        endpoint.Endpoint
+	GetFilesByParentID endpoint.Endpoint
+	UpdateFile         endpoint.Endpoint
+	DeleteFile         endpoint.Endpoint
+}
+
+// MakeEndpoints initializes all Go kit endpoints for file operations
+func MakeEndpoints(s file.Service) Endpoints {
+	return Endpoints{
+		CreateFile:         makeCreateFileEndpoint(s),
+		GetFileByID:        makeGetFileByIDEndpoint(s),
+		GetFilesByParentID: makeGetFilesByParentIDEndpoint(s),
+		UpdateFile:         makeUpdateFileEndpoint(s),
+		DeleteFile:         makeDeleteFileEndpoint(s),
+	}
+}
+
+func makeCreateFileEndpoint(s file.Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req := request.(CreateFileRequest)
+		f := file.File{
+			Name:       req.Name,
+			FolderID:   req.FolderID,
+			OwnerID:    req.OwnerID,
+			Size:       req.Size,
+			Type:       req.Type,
+			ObjectPath: req.Path,
+		}
+		id, err := s.CreateFile(ctx, &f)
+		return CreateFileResponse{ID: *id}, err
+	}
+}
+
+func makeGetFileByIDEndpoint(s file.Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req := request.(GetFileByIDRequest)
+		f, err := s.GetFileByID(ctx, req.ID)
+		return GetFileByIDResponse{
+			ID:        f.ID,
+			Name:      f.Name,
+			FolderID:  f.FolderID,
+			Size:      f.Size,
+			Type:      f.Type,
+			Path:      f.ObjectPath,
+			CreatedAt: f.CreatedAt.String(),
+			UpdatedAt: f.UpdatedAt.String(),
+			Tags:      f.Tags,
+		}, err
+	}
+}
+
+func makeGetFilesByParentIDEndpoint(s file.Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req := request.(GetFilesByFolderIDRequest)
+		files, err := s.GetFilesByFolderID(ctx, req.FolderID)
+		length := len(files) // Used two times
+		shortFiles := make([]ShortFileInfo, length)
+		for i, f := range files {
+			shortFiles[i] = ShortFileInfo{
+				ID:   f.ID,
+				Name: f.Name,
+				Type: f.Type,
+			}
+		}
+		return GetFilesByFolderIDResponse{
+			lenght: length,
+			Files:  shortFiles,
+		}, err
+	}
+}
+
+func makeUpdateFileEndpoint(s file.Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req := request.(UpdateFileRequest)
+		f := file.File{
+			ID:       req.ID,
+			Name:     req.Name,
+			FolderID: req.FolderID,
+		}
+		ok, err := s.UpdateFile(ctx, &f)
+		return UpdateFileResponse{Ok: ok}, err
+	}
+}
+
+func makeDeleteFileEndpoint(s file.Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req := request.(DeleteFileRequest)
+		ok, err := s.DeleteFile(ctx, req.ID)
+		return DeleteFileResponse{Ok: ok}, err
+	}
+}
---
+++ internal/explorer/api/http/file/schemas.go
@@ -0,0 +1,56 @@
+package file
+
+type (
+	CreateFileRequest struct {
+		Name     string `json:"name" validate:"required"`
+		Type     string `json:"type"`
+		FolderID int64  `json:"folder_id"`
+		OwnerID  string `json:"owner_id"`
+		Path     string `json:"path"`
+		Size     int
+	}
+	CreateFileResponse struct {
+		ID int64 `json:"id"`
+	}
+	GetFileByIDRequest struct {
+		ID int64 `json:"id" validate:"required"`
+	}
+	GetFileByIDResponse struct {
+		ID        int64    `json:"id"`
+		Name      string   `json:"name"`
+		Type      string   `json:"type"`
+		Size      int      `json:"size"`
+		FolderID  int64    `json:"folder_id"`
+		Path      string   `json:"path"`
+		CreatedAt string   `json:"created_at"`
+		UpdatedAt string   `json:"updated_at"`
+		Tags      []string `json:"tags"`
+	}
+	GetFilesByFolderIDRequest struct {
+		FolderID int64 `json:"folder_id" validate:"required"`
+	}
+	ShortFileInfo struct {
+		ID   int64  `json:"id"`
+		Name string `json:"name"`
+		Type string `json:"type"`
+	}
+
+	GetFilesByFolderIDResponse struct {
+		lenght int
+		Files  []ShortFileInfo `json:"files"`
+	}
+	UpdateFileRequest struct {
+		ID       int64  `json:"id" validate:"required"`
+		Name     string `json:"name" validate:"required"`
+		FolderID int64  `json:"folder_id"`
+	}
+	UpdateFileResponse struct {
+		Ok bool `json:"ok"`
+	}
+	DeleteFileRequest struct {
+		ID int64 `json:"id" validate:"required"`
+	}
+	DeleteFileResponse struct {
+		Ok bool `json:"ok"`
+	}
+)
