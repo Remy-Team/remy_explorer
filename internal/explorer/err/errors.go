b3854844afdc83d53c5037241de419b127f56fa6
Fascinator <fascinator2904@gmail.com>
2024-06-02 02:22:52 +0300

    Add new error type

--- internal/explorer/err/errors.go
+++ internal/explorer/err/errors.go
@@ -10,3 +10,11 @@ type NotFound struct {
 func (e *NotFound) Error() string {
 	return fmt.Sprintf("Resource with ID %s not found", e.ID)
 }
+
+type DuplicateError struct {
+	str string
+}
+
+func (e *DuplicateError) Error() string {
+	return fmt.Sprintf("Resource %s duplicated", e.str)
+}
